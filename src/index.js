ff344dfcd3f0c9b3dd8513c8914551da248fe7db
Adam K Dean <adamkdean@googlemail.com>
2021-11-14 01:49:14 +0000

    feat: add working express/hls stream server

--- src/index.js
+++ src/index.js
@@ -2,44 +2,123 @@
 // governed by an MIT-style license that can be found at
 // https://opensource.org/licenses/MIT.
 
-// using node.js generate an image 800 x 600
-// with a background of black and a white square
-// in the middle.
-
-const { createWriteStream } = require('fs')
-const { createCanvas } = require('canvas')
+const express = require('express')
+const fs = require('fs')
+const hls = require('hls-server')
 const path = require('path')
 
-const outputImagePath = path.join(__dirname, '..', 'out', 'output.png')
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-const width = 400
-const height = 300
 
-const main = async() => {
-  const canvas = createCanvas(width, height)
-  const ctx = canvas.getContext('2d')
-  const start = Date.now()
+const htmlDir = path.join(__dirname, '../static/html')
+const videoDir = path.join(__dirname, '../static/videos')
 
-  while (true) {
-    const elapsed = Date.now() - start
+const app = express()
+app.get('/', (req, res) => {
+  const fileContents = fs.readFileSync(`${htmlDir}/index.html`)
+  res.status(200).send(fileContents.toString())
+})
+app.use((req, res, next) => {
+  console.log('404 handler', req.url)
+  res.status(404).send('404')
+})
 
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+const server = app.listen(8000, () => console.log('HTTP Server listening on port 8000'))
 
-    ctx.fillStyle = 'white'
-    ctx.font = '24px Hack'
+new hls(server, {
+  provider: {
+    exists: async (req, cb) => {
+      console.log('->', req.url)
 
-    const metrics = ctx.measureText(`${elapsed} ms`)
-    const x = canvas.width / 2 - metrics.width / 2
-    const y = canvas.height / 2 + metrics.actualBoundingBoxAscent / 2
-    ctx.fillText(`${elapsed} ms`, x, y)
+      // if req.url starts with /video/
+      if (!req.url.startsWith('/v/')) return cb(null, true)
+      if (!req.url.endsWith('.m3u8')) return cb(null, true)
 
-    const out = createWriteStream(outputImagePath)
-    const stream = canvas.createPNGStream()
-    stream.pipe(out)
+      // Get filename from req.url without extension or path
+      const filename = req.url.split('/').pop().split('.')[0]
+      const mp4 = `${videoDir}/${filename}.mp4`
+      const m3u8 = `${videoDir}/${filename}/${filename}.m3u8`
 
-    await sleep(1000)
+      // check if mp4 exists
+      const exists = await new Promise((resolve, reject) => {
+        fs.access(mp4, fs.constants.F_OK, (err) => {
+          if (err) return reject(err)
+          resolve(true)
+        })
+      })
+
+      if (!exists) {
+        console.log(`error: requested video ${mp4} does not exist`)
+        return cb(null, false)
+      }
+
+      // check if directory filename exists
+      console.log(`checking if ${videoDir}/${filename} exists`)
+      fs.stat(`${videoDir}/${filename}`, async (err, stats) => {
+        if (err || !stats.isDirectory()) {
+          console.log(`${videoDir}/${filename} does not exist, processing`)
+
+          // ensure directory exists
+          await fs.promises.mkdir(`${videoDir}/${filename}`, { recursive: true })
+
+          // convert mp4 into m3u8 and ts files
+          console.log(`converting ${mp4} to ${m3u8}`)
+          const cmd = `ffmpeg -i ${mp4} -hls_time 5 -f hls ${m3u8}`
+          const exec = require('child_process').exec
+
+          await new Promise((resolve, reject) => {
+            console.log('cmd', cmd)
+            exec(cmd, (err, stdout, stderr) => {
+              if (err) {
+                console.error(err)
+                return reject(err)
+              }
+              console.log(stdout)
+              console.log(stderr)
+              resolve()
+            })
+          })
+
+          console.log(`prepending ${filename} to all lines in ${m3u8}`)
+          const m3u8Contents = fs.readFileSync(m3u8).toString()
+          const newM3u8Contents = m3u8Contents.split('\n').map(line => {
+            if (line.startsWith(filename)) return `${filename}/${line}`
+            return line
+          }).join('\n')
+          fs.writeFileSync(m3u8, newM3u8Contents)
+
+          console.log('generated m3u8 playlist', m3u8)
+        }
+
+        console.log('checking video playlist exists')
+        fs.access(`${videoDir}/${filename}/${filename}.m3u8`, fs.constants.F_OK, (err) => {
+          if (err) {
+            console.error(err)
+            return cb(null, false)
+          }
+          console.log('video playlist exists')
+          return cb(null, true)
+        })
+      })
+    },
+    getManifestStream: (req, cb) => {
+      console.log('-> get manifest', req.url)
+      const filename = req.url.split('/').pop().split('.')[0]
+      const m3u8 = `${videoDir}/${filename}/${filename}.m3u8`
+
+      console.log('<- returning manifest:', m3u8)
+      cb(null, fs.createReadStream(m3u8))
+    },
+    getSegmentStream: (req, cb) => {
+      console.log('-> get segment', req.url)
+
+      // from req.url as format /v/earth_1920_1080/earth_1920_10800.ts extract last two parts
+      const urlParts = req.url.split('/')
+      const filename = urlParts.pop().split('.')[0]
+      const video = urlParts.pop()
+      const ts = `${videoDir}/${video}/${filename}.ts`
+
+      console.log('<- returning segment:', ts)
+      cb(null, fs.createReadStream(ts))
+    }
   }
-}
-
-main()
+})
